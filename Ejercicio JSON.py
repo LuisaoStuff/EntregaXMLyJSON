4982625355cd462a13d69b43329fa3ecac15723a
Luis Vazquez Alejo <luisvazquezalejo@gmail.com>
2019-03-22 00:03:52 +0100

    Termino el ejercicio 5

--- Ejercicio JSON.py
+++ Ejercicio JSON.py
@@ -4,6 +4,7 @@
 
 import json
 from os import system
+from datetime import datetime	#	Importo esta librería para validar la fecha en el ultimo ejercicio
 
 ########################################################################
 #						      Funciones							       #
@@ -40,8 +41,40 @@ def BuscadorEnSinopsis(C1,C2,Catalogo):		#	Recibe 2 palabras por teclado. Compru
 			lista.append(pelicula["title"])
 	return lista
 
-def BuscadorPorActor(Actor,Catalogo):
-	
+def ValidarActor(Actor,Catalogo):		#	Recibe un actor y devuelve verdadero si lo encuentra
+	indicador=False
+	for pelicula in Catalogo:
+		if Actor in pelicula["actors"]:
+			indicador=True
+	return indicador
+
+
+def Filmografia(Actor,Catalogo):		#	Recibe un actor y añade a una lista las peliculas en las que aparece.
+	lista=[]
+	for pelicula in Catalogo:
+		if Actor in pelicula["actors"]:
+			lista.append(pelicula["title"])
+	return lista
+
+def ValidarFecha(Fecha):
+	try:
+		Fecha = datetime.strptime(Fecha, '%Y-%m-%d')
+		return True
+	except:
+		print("\n			Introduce una fecha correcta")
+		return False
+
+def FiltrarPorFechas(F1,F2,Catalogo):
+	lista=[]
+	F1=int(F1.replace("-",""))
+	F2=int(F2.replace("-",""))
+	for pelicula in Catalogo:
+		Fecha=int(pelicula["releaseDate"].replace("-",""))
+		if F1<Fecha and F2>Fecha:
+			lista.append(pelicula["title"])
+	return lista
+
+
 
 ########################################################################
 #						   Código Principal							   #
@@ -96,23 +129,43 @@ with open("movies.json","r") as fichero:
 				C1=input("			Coincidencia 1 >>> ").upper()
 				C2=input("			Coincidencia 2 >>> ").upper()
 				lista=BuscadorEnSinopsis(C1,C2,Catalogo)
-				print("\n			Se han econtrado las siguientes películas:\n")
-				try:
-					if len(lista)>0:
-						print("\n			Se han econtrado las siguientes películas:\n")
-						for pelicula in lista:
-							print("			>",pelicula)
-				except:
+				if len(lista)>0:
+					print("\n			Se han econtrado las siguientes películas:\n")
+					for pelicula in lista:
+						print("			>",pelicula)
+				else:
 					print("\n			No se han econtrado películas\n")
 				Pausa()
 
 			if opcion==4:
 				clear(10)
 				print("			Buscador por actor:")		
-				Actor=input("			Nombre >>> ").upper().replace(" ","").replace(".","")
-
+				Actor=input("			Nombre >>> ").title()
+				if ValidarActor(Actor,Catalogo):
+					print("			",Actor,"aparece en:")
+					for pelicula in Filmografia(Actor,Catalogo):
+						print("			>",pelicula)
+				else:
+					print("			",Actor,"no aparece en la lista.")
 				Pausa()
 
 			if opcion==5:
+				clear(10)
+				print("			El formato es YYYY-MM-DD\n\n")
+				
+				print("			Primera fecha:")		#	\
+				F1=input("			Fecha >>> ")		#	|
+				while ValidarFecha(F1)==False:			#	|
+					F1=input("			Fecha >>> ")	#	|	Valido el formato de las fechas
+				print("			Segunda fecha:")		#	|
+				F2=input("			Fecha >>> ")		#	|
+				while ValidarFecha(F2)==False:			#	|
+					F2=input("			Fecha >>> ")	#	/
+				try:
+					print("			Se han encontrado",len(FiltrarPorFechas(F1,F2,Catalogo)),"películas:")	#	Si la lista devuelta por la funcion
+					for pelicula in FiltrarPorFechas(F1,F2,Catalogo):										#	tiene longitud, la imprime
+						print("			> ",pelicula)
+				except:
+					print("			No se han encontrado peliculas")			#	Si no, significa que no ha encontrado ninguna pelicula
 
 				Pausa()
